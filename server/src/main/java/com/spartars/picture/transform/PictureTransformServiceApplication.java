df3fda4512c32a0c62cd45e7f3fda3a00ca310e0
spartars <spartars@yandex.ru>
2020-05-12 20:03:44 +0300

    Init server

---
+++ server/src/main/java/com/spartars/picture/transform/PictureTransformServiceApplication.java
@@ -0,0 +1,13 @@
+package com.spartars.picture.transform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PictureTransformServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PictureTransformServiceApplication.class, args);
+    }
+
+}
